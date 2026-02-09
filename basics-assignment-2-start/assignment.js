1a785d5e469cef9e673de420d98793655cbfbbd5
fiati3ewty <fiati3ewty@gmail.com>
2026-02-09 23:12:34 +0700

    Feat: Complete assignment "Assignment 2: Time to Practice: Functions"

---
+++ basics-assignment-2-start/assignment.js
@@ -0,0 +1,28 @@
+// Assignment #1
+function alertName() {
+    alert("What you name ?");
+}
+
+function getName(name) {
+    alert(`You name is ${name}`);
+}
+
+// Assignment #2
+alertName();
+getName("Choco");
+
+// Assignment #3
+const task3Element = document.getElementById('task-3');
+task3Element.addEventListener('click', alertName);
+
+// Assignment #4
+function brandNew(txt1, txt2, txt3) {
+    alert(txt1 + txt2 + txt3);
+}
+
+// Assingment #5
+brandNew("Shert", "Skert", "Jense");
+
+
+
+
